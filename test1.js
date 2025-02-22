7f47b4cf1bab5107ab5e0e5caedbab7b12d80107
DerixPro <ivanblognow@gmail.com>
2025-02-23 02:54:27 +0300

    site

---
+++ test1.js
@@ -0,0 +1,529 @@
+
+const showbtn1 = document.getElementById("n1");
+const showbtn2 = document.getElementById("n2");
+const showbtn3 = document.getElementById("n3");
+const showbtn4 = document.getElementById("n4");
+const showbtn5 = document.getElementById("n5");
+const showbtn6 = document.getElementById("n6");
+
+const showbtn7 = document.getElementById("n7");
+const showbtn8 = document.getElementById("n8");
+const showbtn9 = document.getElementById("n9");
+const showbtn10 = document.getElementById("n10");
+const showbtn11 = document.getElementById("n11");
+const showbtn12 = document.getElementById("n12");
+
+const showbtn13 = document.getElementById("n13");
+const showbtn14 = document.getElementById("n14");
+const showbtn15 = document.getElementById("n15");
+const showbtn16 = document.getElementById("n16");
+const showbtn17 = document.getElementById("n17");
+const showbtn18 = document.getElementById("n18");
+
+const showbtn19 = document.getElementById("n19");
+const showbtn20 = document.getElementById("n20");
+const showbtn21 = document.getElementById("n21");
+const showbtn22 = document.getElementById("n22");
+const showbtn23 = document.getElementById("n23");
+const showbtn24 = document.getElementById("n24");
+
+const showbtn25 = document.getElementById("n25");
+const showbtn26 = document.getElementById("n26");
+const showbtn27 = document.getElementById("n27");
+const showbtn28 = document.getElementById("n28");
+const showbtn29 = document.getElementById("n29");
+const showbtn30 = document.getElementById("n30");
+
+const showbtn31 = document.getElementById("n31");
+const showbtn32 = document.getElementById("n32");
+const showbtn33 = document.getElementById("n33");
+const showbtn34 = document.getElementById("n34");
+const showbtn35 = document.getElementById("n35");
+const showbtn36 = document.getElementById("n36");
+
+const showbtn37 = document.getElementById("n37");
+const showbtn38 = document.getElementById("n38");
+const showbtn39 = document.getElementById("n39");
+const showbtn40 = document.getElementById("n40");
+const showbtn41 = document.getElementById("n41");
+const showbtn42 = document.getElementById("n42");
+
+const showbtn43 = document.getElementById("n43");
+const showbtn44 = document.getElementById("n44");
+const showbtn45 = document.getElementById("n45");
+const showbtn46 = document.getElementById("n46");
+const showbtn47 = document.getElementById("n47");
+const showbtn48 = document.getElementById("n48");
+
+
+
+const closebtn1 = document.getElementById("closebtn1");
+const closebtn2 = document.getElementById("closebtn2");
+const closebtn3 = document.getElementById("closebtn3");
+const closebtn4 = document.getElementById("closebtn4");
+const closebtn5 = document.getElementById("closebtn5");
+const closebtn6 = document.getElementById("closebtn6");
+
+const closebtn7 = document.getElementById("closebtn7");
+const closebtn8 = document.getElementById("closebtn8");
+const closebtn9 = document.getElementById("closebtn9");
+const closebtn10 = document.getElementById("closebtn10");
+const closebtn11 = document.getElementById("closebtn11");
+const closebtn12 = document.getElementById("closebtn12");
+
+const closebtn13 = document.getElementById("closebtn13");
+const closebtn14 = document.getElementById("closebtn14");
+const closebtn15 = document.getElementById("closebtn15");
+const closebtn16 = document.getElementById("closebtn16");
+const closebtn17 = document.getElementById("closebtn17");
+const closebtn18 = document.getElementById("closebtn18");
+
+const closebtn19 = document.getElementById("closebtn19");
+const closebtn20 = document.getElementById("closebtn20");
+const closebtn21 = document.getElementById("closebtn21");
+const closebtn22 = document.getElementById("closebtn22");
+const closebtn23 = document.getElementById("closebtn23");
+const closebtn24 = document.getElementById("closebtn24");
+
+const closebtn25 = document.getElementById("closebtn25");
+const closebtn26 = document.getElementById("closebtn26");
+const closebtn27 = document.getElementById("closebtn27");
+const closebtn28 = document.getElementById("closebtn28");
+const closebtn29 = document.getElementById("closebtn29");
+const closebtn30 = document.getElementById("closebtn30");
+
+const closebtn31 = document.getElementById("closebtn31");
+const closebtn32 = document.getElementById("closebtn32");
+const closebtn33 = document.getElementById("closebtn33");
+const closebtn34 = document.getElementById("closebtn34");
+const closebtn35 = document.getElementById("closebtn35");
+const closebtn36 = document.getElementById("closebtn36");
+
+const closebtn37 = document.getElementById("closebtn37");
+const closebtn38 = document.getElementById("closebtn38");
+const closebtn39 = document.getElementById("closebtn39");
+const closebtn40 = document.getElementById("closebtn40");
+const closebtn41 = document.getElementById("closebtn41");
+const closebtn42 = document.getElementById("closebtn42");
+
+const closebtn43 = document.getElementById("closebtn43");
+const closebtn44 = document.getElementById("closebtn44");
+const closebtn45 = document.getElementById("closebtn45");
+const closebtn46 = document.getElementById("closebtn46");
+const closebtn47 = document.getElementById("closebtn47");
+const closebtn48 = document.getElementById("closebtn48");
+
+
+const popup1 = document.getElementById("window1");
+const popup2 = document.getElementById("window2");
+const popup3 = document.getElementById("window3");
+const popup4 = document.getElementById("window4");
+const popup5 = document.getElementById("window5");
+const popup6 = document.getElementById("window6");
+
+const popup7 = document.getElementById("window7");
+const popup8 = document.getElementById("window8");
+const popup9 = document.getElementById("window9");
+const popup10 = document.getElementById("window10");
+const popup11 = document.getElementById("window11");
+const popup12 = document.getElementById("window12");
+
+const popup13 = document.getElementById("window13");
+const popup14 = document.getElementById("window14");
+const popup15 = document.getElementById("window15");
+const popup16 = document.getElementById("window16");
+const popup17 = document.getElementById("window17");
+const popup18 = document.getElementById("window18");
+
+const popup19 = document.getElementById("window19");
+const popup20 = document.getElementById("window20");
+const popup21 = document.getElementById("window21");
+const popup22 = document.getElementById("window22");
+const popup23 = document.getElementById("window23");
+const popup24 = document.getElementById("window24");
+
+const popup25 = document.getElementById("window25");
+const popup26 = document.getElementById("window26");
+const popup27 = document.getElementById("window27");
+const popup28 = document.getElementById("window28");
+const popup29 = document.getElementById("window29");
+const popup30 = document.getElementById("window30");
+
+const popup31 = document.getElementById("window31");
+const popup32 = document.getElementById("window32");
+const popup33 = document.getElementById("window33");
+const popup34 = document.getElementById("window34");
+const popup35 = document.getElementById("window35");
+const popup36 = document.getElementById("window36");
+
+const popup37 = document.getElementById("window37");
+const popup38 = document.getElementById("window38");
+const popup39 = document.getElementById("window39");
+const popup40 = document.getElementById("window40");
+const popup41 = document.getElementById("window41");
+const popup42 = document.getElementById("window42");
+
+const popup43 = document.getElementById("window43");
+const popup44 = document.getElementById("window44");
+const popup45 = document.getElementById("window45");
+const popup46 = document.getElementById("window46");
+const popup47 = document.getElementById("window47");
+const popup48 = document.getElementById("window48");
+
+
+
+
+showbtn1.addEventListener("click", function(){
+   popup1.classList.add("activ");
+})
+showbtn2.addEventListener("click", function(){
+   popup2.classList.add("activ");
+})
+showbtn3.addEventListener("click", function(){
+   popup3.classList.add("activ");
+})
+showbtn4.addEventListener("click", function(){
+   popup4.classList.add("activ");
+})
+showbtn5.addEventListener("click", function(){
+   popup5.classList.add("activ");
+})
+showbtn6.addEventListener("click", function(){
+   popup6.classList.add("activ");
+})
+
+showbtn7.addEventListener("click", function(){
+   popup7.classList.add("activ");
+})
+showbtn8.addEventListener("click", function(){
+   popup8.classList.add("activ");
+})
+showbtn9.addEventListener("click", function(){
+   popup9.classList.add("activ");
+})
+showbtn10.addEventListener("click", function(){
+   popup10.classList.add("activ");
+})
+showbtn11.addEventListener("click", function(){
+   popup11.classList.add("activ");
+})
+showbtn12.addEventListener("click", function(){
+   popup12.classList.add("activ");
+})
+
+showbtn13.addEventListener("click", function(){
+   popup13.classList.add("activ");
+})
+showbtn14.addEventListener("click", function(){
+   popup14.classList.add("activ");
+})
+showbtn15.addEventListener("click", function(){
+   popup15.classList.add("activ");
+})
+showbtn16.addEventListener("click", function(){
+   popup16.classList.add("activ");
+})
+showbtn17.addEventListener("click", function(){
+   popup17.classList.add("activ");
+})
+showbtn18.addEventListener("click", function(){
+   popup18.classList.add("activ");
+})
+
+showbtn19.addEventListener("click", function(){
+   popup19.classList.add("activ");
+})
+showbtn20.addEventListener("click", function(){
+   popup20.classList.add("activ");
+})
+showbtn21.addEventListener("click", function(){
+   popup21.classList.add("activ");
+})
+showbtn22.addEventListener("click", function(){
+   popup22.classList.add("activ");
+})
+showbtn23.addEventListener("click", function(){
+   popup23.classList.add("activ");
+})
+showbtn24.addEventListener("click", function(){
+   popup24.classList.add("activ");
+})
+
+showbtn25.addEventListener("click", function(){
+   popup25.classList.add("activ");
+})
+showbtn26.addEventListener("click", function(){
+   popup26.classList.add("activ");
+})
+showbtn27.addEventListener("click", function(){
+   popup27.classList.add("activ");
+})
+showbtn28.addEventListener("click", function(){
+   popup28.classList.add("activ");
+})
+showbtn29.addEventListener("click", function(){
+   popup29.classList.add("activ");
+})
+showbtn30.addEventListener("click", function(){
+   popup30.classList.add("activ");
+})
+
+showbtn31.addEventListener("click", function(){
+   popup31.classList.add("activ");
+})
+showbtn32.addEventListener("click", function(){
+   popup32.classList.add("activ");
+})
+showbtn33.addEventListener("click", function(){
+   popup33.classList.add("activ");
+})
+showbtn34.addEventListener("click", function(){
+   popup34.classList.add("activ");
+})
+showbtn35.addEventListener("click", function(){
+   popup35.classList.add("activ");
+})
+showbtn36.addEventListener("click", function(){
+   popup36.classList.add("activ");
+})
+
+showbtn37.addEventListener("click", function(){
+   popup37.classList.add("activ");
+})
+showbtn38.addEventListener("click", function(){
+   popup38.classList.add("activ");
+})
+showbtn39.addEventListener("click", function(){
+   popup39.classList.add("activ");
+})
+showbtn40.addEventListener("click", function(){
+   popup40.classList.add("activ");
+})
+showbtn41.addEventListener("click", function(){
+   popup41.classList.add("activ");
+})
+showbtn42.addEventListener("click", function(){
+   popup42.classList.add("activ");
+})
+
+showbtn43.addEventListener("click", function(){
+   popup43.classList.add("activ");
+})
+showbtn44.addEventListener("click", function(){
+   popup44.classList.add("activ");
+})
+showbtn45.addEventListener("click", function(){
+   popup45.classList.add("activ");
+})
+showbtn46.addEventListener("click", function(){
+   popup46.classList.add("activ");
+})
+showbtn47.addEventListener("click", function(){
+   popup47.classList.add("activ");
+})
+showbtn48.addEventListener("click", function(){
+   popup48.classList.add("activ");
+})
+
+
+
+
+closebtn1.addEventListener("click", () => {
+    popup1.classList.remove("activ");
+
+})
+closebtn2.addEventListener("click", () => {
+    popup2.classList.remove("activ");
+
+})
+closebtn3.addEventListener("click", () => {
+    popup3.classList.remove("activ");
+
+})
+closebtn4.addEventListener("click", () => {
+    popup4.classList.remove("activ");
+
+})
+closebtn5.addEventListener("click", () => {
+    popup5.classList.remove("activ");
+
+})
+closebtn6.addEventListener("click", () => {
+    popup6.classList.remove("activ");
+
+})
+
+closebtn7.addEventListener("click", () => {
+    popup7.classList.remove("activ");
+
+})
+closebtn8.addEventListener("click", () => {
+    popup8.classList.remove("activ");
+
+})
+closebtn9.addEventListener("click", () => {
+    popup9.classList.remove("activ");
+
+})
+closebtn10.addEventListener("click", () => {
+    popup10.classList.remove("activ");
+
+})
+closebtn11.addEventListener("click", () => {
+    popup11.classList.remove("activ");
+
+})
+closebtn12.addEventListener("click", () => {
+    popup12.classList.remove("activ");
+
+})
+
+closebtn13.addEventListener("click", () => {
+    popup13.classList.remove("activ");
+
+})
+closebtn14.addEventListener("click", () => {
+    popup14.classList.remove("activ");
+
+})
+closebtn15.addEventListener("click", () => {
+    popup15.classList.remove("activ");
+
+})
+closebtn16.addEventListener("click", () => {
+    popup16.classList.remove("activ");
+
+})
+closebtn17.addEventListener("click", () => {
+    popup17.classList.remove("activ");
+
+})
+closebtn18.addEventListener("click", () => {
+    popup18.classList.remove("activ");
+
+})
+
+closebtn19.addEventListener("click", () => {
+    popup19.classList.remove("activ");
+
+})
+closebtn20.addEventListener("click", () => {
+    popup20.classList.remove("activ");
+
+})
+closebtn21.addEventListener("click", () => {
+    popup21.classList.remove("activ");
+
+})
+closebtn22.addEventListener("click", () => {
+    popup22.classList.remove("activ");
+
+})
+closebtn23.addEventListener("click", () => {
+    popup23.classList.remove("activ");
+
+})
+closebtn24.addEventListener("click", () => {
+    popup24.classList.remove("activ");
+
+})
+
+closebtn25.addEventListener("click", () => {
+    popup25.classList.remove("activ");
+
+})
+closebtn26.addEventListener("click", () => {
+    popup26.classList.remove("activ");
+
+})
+closebtn27.addEventListener("click", () => {
+    popup27.classList.remove("activ");
+
+})
+closebtn28.addEventListener("click", () => {
+    popup28.classList.remove("activ");
+
+})
+closebtn29.addEventListener("click", () => {
+    popup29.classList.remove("activ");
+
+})
+closebtn30.addEventListener("click", () => {
+    popup30.classList.remove("activ");
+
+})
+
+closebtn31.addEventListener("click", () => {
+    popup31.classList.remove("activ");
+
+})
+closebtn32.addEventListener("click", () => {
+    popup32.classList.remove("activ");
+
+})
+closebtn33.addEventListener("click", () => {
+    popup33.classList.remove("activ");
+
+})
+closebtn34.addEventListener("click", () => {
+    popup34.classList.remove("activ");
+
+})
+closebtn35.addEventListener("click", () => {
+    popup35.classList.remove("activ");
+
+})
+closebtn36.addEventListener("click", () => {
+    popup36.classList.remove("activ");
+
+})
+
+closebtn37.addEventListener("click", () => {
+    popup37.classList.remove("activ");
+
+})
+closebtn39.addEventListener("click", () => {
+    popup39.classList.remove("activ");
+
+})
+closebtn40.addEventListener("click", () => {
+    popup40.classList.remove("activ");
+
+})
+closebtn41.addEventListener("click", () => {
+    popup41.classList.remove("activ");
+
+})
+closebtn42.addEventListener("click", () => {
+    popup42.classList.remove("activ");
+
+})
+closebtn43.addEventListener("click", () => {
+    popup43.classList.remove("activ");
+
+})
+
+closebtn44.addEventListener("click", () => {
+    popup44.classList.remove("activ");
+
+})
+closebtn45.addEventListener("click", () => {
+    popup45.classList.remove("activ");
+
+})
+closebtn46.addEventListener("click", () => {
+    popup46.classList.remove("activ");
+
+})
+closebtn47.addEventListener("click", () => {
+    popup47.classList.remove("activ");
+
+})
+closebtn48.addEventListener("click", () => {
+    popup48.classList.remove("activ");
+
+})
+closebtn38.addEventListener("click", () => {
+    popup38.classList.remove("activ");
+
+})
